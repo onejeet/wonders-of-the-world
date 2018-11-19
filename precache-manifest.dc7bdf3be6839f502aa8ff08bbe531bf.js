f75f22ff99e5905da638f2c41994b7c33211f149
onejeet <jitendra909@gmail.com>
2018-11-19 11:36:06 +0530

    Updates

---
+++ precache-manifest.dc7bdf3be6839f502aa8ff08bbe531bf.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = [
+  {
+    "revision": "8659515094d85059439811bbedcbbc39",
+    "url": "/wonders-of-the-world-react/static/media/hamburger.86595150.svg"
+  },
+  {
+    "revision": "8f49da55183b5d825482",
+    "url": "/wonders-of-the-world-react/static/css/main.dd6244c9.chunk.css"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/wonders-of-the-world-react/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "709d017c2bea3b284f73",
+    "url": "/wonders-of-the-world-react/static/js/1.709d017c.chunk.js"
+  },
+  {
+    "revision": "9d08005cb3d81e07105d",
+    "url": "/wonders-of-the-world-react/static/js/runtime~main.9d08005c.js"
+  },
+  {
+    "revision": "1d16aea1abfe5ddd95d0bfa5239dfc3b",
+    "url": "/wonders-of-the-world-react/static/media/logo.1d16aea1.svg"
+  },
+  {
+    "revision": "8f49da55183b5d825482",
+    "url": "/wonders-of-the-world-react/static/js/main.8f49da55.chunk.js"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/wonders-of-the-world-react/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/wonders-of-the-world-react/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/wonders-of-the-world-react/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/wonders-of-the-world-react/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "709d017c2bea3b284f73",
+    "url": "/wonders-of-the-world-react/static/css/1.6e6af794.chunk.css"
+  },
+  {
+    "revision": "5529ddbe9c50c8ec50bbd0c76e7ebb5a",
+    "url": "/wonders-of-the-world-react/index.html"
+  }
+];
